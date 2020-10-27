cb47da437f195084ff361000467efdbbdd950654
ZabdielReyes <70989008+ZabdielReyes@users.noreply.github.com>
2020-10-27 16:26:54 -0600

    Create arreglo.h

---
+++ arreglo.h
@@ -0,0 +1,32 @@
+#ifndef ARREGLO_H
+#define ARREGLO_H
+
+#include <iostream>
+using namespace std;
+
+class ArregloDinamico
+{
+    string *arreglo;
+    size_t tam;
+    size_t cont;
+    const static size_t MAX = 5;
+
+public:
+    ArregloDinamico();
+    ~ArregloDinamico();
+    void insertar_final(int v);
+    void insertar_inicio(int v);
+    void insertar_final(const string &s);
+    void insertar_inicio(const string &s);
+    
+    
+    size_t size();
+    string operator[](size_t p)
+    {
+        return arreglo[p];
+    }
+private:
+    void expandir();
+};
+
+#endif
